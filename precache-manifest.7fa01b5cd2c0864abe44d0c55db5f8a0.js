520d389fbfdd0894483e9da87c8f2f8efec8e830
orhannurkan <orhannurkan@hotmail.com>
2019-11-29 15:39:15 +0100

    Updates

---
+++ precache-manifest.7fa01b5cd2c0864abe44d0c55db5f8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461de6e56ac26c2aeadef4f4cd6b4a75",
+    "url": "/reactDefault/index.html"
+  },
+  {
+    "revision": "3c476e2251adb265c398",
+    "url": "/reactDefault/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0d15ba118d02382891d7",
+    "url": "/reactDefault/static/js/2.48d77163.chunk.js"
+  },
+  {
+    "revision": "3c476e2251adb265c398",
+    "url": "/reactDefault/static/js/main.f6eb49e3.chunk.js"
+  },
+  {
+    "revision": "418a442d7f270f29aa50",
+    "url": "/reactDefault/static/js/runtime-main.7117af0b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactDefault/static/media/logo.25bf045c.svg"
+  }
+]);
